d5d8150f6bf30f9c0d605e8cc44407f96f50c00f
Leon Sievers <leon.sievers@tum.de>
2020-03-12 17:57:22 +0100

    Added optimizer

--- replyviech/main.py
+++ replyviech/main.py
@@ -1,3 +1,4 @@
 from replyviech.paser import parse_input
 from replyviech.util import calculate_potential
 from replyviech.workers import Developer, Manager
+
--- replyviech/workers.py
+++ replyviech/workers.py
@@ -1,11 +1,13 @@
 class Developer:
-    def __init__(self, company, bonus, skillset):
+    def __init__(self, id, company, bonus, skillset):
+        self.id = id
         self.company = company
         self.bonus = bonus
         self.skillset = skillset
 
 
 class Manager:
-    def __init__(self, company, bonus):
+    def __init__(self, id, company, bonus):
+        self.id = id
         self.company = company
         self.bonus = bonus
